0b088514dc789637b9d18f2f572430a55a4d1b14
Matthias Jordan <me@iammatthias.com>
2019-02-01 14:20:00 -0800

    Attempted fix for static builds

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,3 +1,18 @@
+exports.onCreateWebpackConfig = ({ stage, loaders, actions }) => {
+  if (stage === 'build-html') {
+    actions.setWebpackConfig({
+      module: {
+        rules: [
+          {
+            test: /react-images/,
+            use: loaders.null(),
+          },
+        ],
+      },
+    });
+  }
+};
+
 const path = require(`path`)
 
 exports.createPages = ({ graphql, actions }) => {
