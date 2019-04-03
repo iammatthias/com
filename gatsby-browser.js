88cf215bdeb44b19ecf50ab213cb03b1121249a3
Matthias Jordan <matthias.k.jordan@gmail.com>
2019-04-03 11:19:08 -0700

    Update gatsby-browser.js

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -25,7 +25,8 @@ export const shouldUpdateScroll = ({
 
 
 export const onClientEntry = async () => {
-  if (typeof IntersectionObserver === `undefined`) {
-    await import(`intersection-observer`);
+  if (typeof IntersectionObserver === "undefined") {
+    await import("intersection-observer");
+    console.log("IntersectionObserver polyfilled ;)");
   }
-}
+};
