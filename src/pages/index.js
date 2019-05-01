f4cf25f7e0b9d0bd59f00ac77f996f9642b62563
Matthias Jordan <matthias.k.jordan@gmail.com>
2019-04-30 18:07:04 -0700

    Little changes

--- src/pages/index.js
+++ src/pages/index.js
@@ -64,7 +64,7 @@ export const query = graphql`
           }
           body {
             childMarkdownRemark {
-              excerpt(pruneLength: 140)
+              excerpt(pruneLength: 140, format: HTML)
             }
           }
         }
