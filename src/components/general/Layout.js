aeea27d313bbf50393f6922e9eea798537d9b0c6
Matthias Jordan <matthias.k.jordan@gmail.com>
2019-05-01 20:26:54 -0700

    PWA assets

--- src/components/general/Layout.js
+++ src/components/general/Layout.js
@@ -22,38 +22,56 @@ const Layout = ({ children, ...props }) => {
           />
           <link
             rel="apple-touch-startup-image"
-            href="/splash/launch-640x1136.png"
-            media="(device-width: 320px) and (device-height: 568px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)"
+            media="(device-width: 414px) and (device-height: 896px) and (-webkit-device-pixel-ratio: 3)"
+            href="/splash/launch-1242x2688.png"
           />
+
           <link
             rel="apple-touch-startup-image"
-            href="/splash/launch-750x1294.png"
-            media="(device-width: 375px) and (device-height: 667px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)"
+            media="(device-width: 414px) and (device-height: 896px) and (-webkit-device-pixel-ratio: 2)"
+            href="/splash/launch-828x1792.png"
           />
+
           <link
             rel="apple-touch-startup-image"
-            href="/splash/launch-1242x2148.png"
-            media="(device-width: 414px) and (device-height: 736px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait)"
-          />
-          <link
-            rel="apple-touch-startup-image"
+            media="(device-width: 375px) and (device-height: 812px) and (-webkit-device-pixel-ratio: 3)"
             href="/splash/launch-1125x2436.png"
-            media="(device-width: 375px) and (device-height: 812px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait)"
           />
+
           <link
             rel="apple-touch-startup-image"
-            href="/splash/launch-1536x2048.png"
-            media="(min-device-width: 768px) and (max-device-width: 1024px) and (-webkit-min-device-pixel-ratio: 2) and (orientation: portrait)"
+            media="(device-width: 414px) and (device-height: 736px) and (-webkit-device-pixel-ratio: 3)"
+            href="/splash/launch-1242x2208.png"
           />
+
           <link
             rel="apple-touch-startup-image"
-            href="/splash/launch-1668x2224.png"
-            media="(min-device-width: 834px) and (max-device-width: 834px) and (-webkit-min-device-pixel-ratio: 2) and (orientation: portrait)"
+            media="(device-width: 375px) and (device-height: 667px) and (-webkit-device-pixel-ratio: 2)"
+            href="/splash/launch-750x1334.png"
           />
+
           <link
             rel="apple-touch-startup-image"
+            media="(device-width: 1024px) and (device-height: 1366px) and (-webkit-device-pixel-ratio: 2)"
             href="/splash/launch-2048x2732.png"
-            media="(min-device-width: 1024px) and (max-device-width: 1024px) and (-webkit-min-device-pixel-ratio: 2) and (orientation: portrait)"
+          />
+
+          <link
+            rel="apple-touch-startup-image"
+            media="(device-width: 834px) and (device-height: 1194px) and (-webkit-device-pixel-ratio: 2)"
+            href="/splash/launch-1668x2388.png"
+          />
+
+          <link
+            rel="apple-touch-startup-image"
+            media="(device-width: 834px) and (device-height: 1112px) and (-webkit-device-pixel-ratio: 2)"
+            href="/splash/launch-1668x2224.png"
+          />
+
+          <link
+            rel="apple-touch-startup-image"
+            media="(device-width: 768px) and (device-height: 1024px) and (-webkit-device-pixel-ratio: 2)"
+            href="/splash/launch-1536x2048.png"
           />
         </Helmet>
         <GlobalStyle />
