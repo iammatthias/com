a2a93ca421fba2c833364695c9b5617a17686f96
Matthias Jordan <matthias.k.jordan@gmail.com>
2019-05-05 10:02:21 -0700

    Prefetch for Google Analytics

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,6 +1,16 @@
 import React from 'react'
 import Layout from './src/components/general/Layout'
 
+exports.onRenderBody = ({ setHeadComponents }) => {
+  setHeadComponents([
+    <link
+      rel="dns-prefetch"
+      key="dns-prefetch-google-analytics"
+      href="https://www.google-analytics.com"
+    />,
+  ])
+}
+
 export const wrapPageElement = ({ element, props }) => {
   return <Layout {...props}>{element}</Layout>
 }
