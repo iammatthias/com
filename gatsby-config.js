577e0f15124b2a77a6313b5c8809d132049b03a8
Matthias Jordan <matthias.k.jordan@gmail.com>
2019-05-05 10:31:39 -0700

    Robots.txt

--- gatsby-config.js
+++ gatsby-config.js
@@ -17,17 +17,18 @@ try {
   }
 }
 
+const {
+  NODE_ENV,
+  URL: NETLIFY_SITE_URL = 'https://iammatthias.com',
+  DEPLOY_PRIME_URL: NETLIFY_DEPLOY_URL = NETLIFY_SITE_URL,
+  CONTEXT: NETLIFY_ENV = NODE_ENV,
+} = process.env
+const isNetlifyProduction = NETLIFY_ENV === 'production'
+const siteUrl = isNetlifyProduction ? NETLIFY_SITE_URL : NETLIFY_DEPLOY_URL
+
 module.exports = {
   siteMetadata: {
     siteUrl: config.siteUrl,
-    rssMetadata: {
-      site_url: config.siteUrl,
-      title: config.siteTitle,
-      description: config.siteDescription,
-      image_url: `${config.siteUrl}${config.siteLogo}`,
-      author: config.author,
-      copyright: config.copyright,
-    },
   },
   plugins: [
     `gatsby-plugin-flow`,
@@ -131,6 +132,28 @@ module.exports = {
         process.env.NODE_ENV === 'development'
           ? contentfulConfig.development
           : contentfulConfig.production,
+    },
+
+    {
+      resolve: 'gatsby-plugin-robots-txt',
+      options: {
+        resolveEnv: () => NETLIFY_ENV,
+        env: {
+          production: {
+            policy: [{ userAgent: '*' }],
+          },
+          'branch-deploy': {
+            policy: [{ userAgent: '*', disallow: ['/'] }],
+            sitemap: null,
+            host: null,
+          },
+          'deploy-preview': {
+            policy: [{ userAgent: '*', disallow: ['/'] }],
+            sitemap: null,
+            host: null,
+          },
+        },
+      },
     },
 
     'gatsby-plugin-catch-links',
