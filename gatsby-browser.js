6f47241a05a29414fc313f8c4c338920fe856a61
Matthias Jordan <matthias.k.jordan@gmail.com>
2019-05-09 18:00:24 -0700

    Small style tweaks

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,30 +1,12 @@
 import React from 'react'
 import Layout from './src/components/general/Layout'
 
-const transitionDelay = 300
-
 export const wrapPageElement = ({ element, props, location }) => {
   return (
     <Layout location={location} {...props}>
       {element}
     </Layout>
   )
-}
-
-export const shouldUpdateScroll = ({
-  routerProps: { location },
-  getSavedScrollPosition,
-}) => {
-  if (location.action === 'PUSH') {
-    window.setTimeout(() => window.scrollTo(0, 0), transitionDelay)
-  } else {
-    const savedPosition = getSavedScrollPosition(location)
-    window.setTimeout(
-      () => window.scrollTo(...(savedPosition || [0, 0])),
-      transitionDelay
-    )
-  }
-  return false
 }
 
 export const onClientEntry = async () => {
