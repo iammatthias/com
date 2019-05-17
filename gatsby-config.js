8e126dccccb585961fcc1fdecd0feaf0af271b59
Matthias Jordan <matthias.k.jordan@gmail.com>
2019-05-17 19:27:55 +0000

    Add image grid to article layout

--- gatsby-config.js
+++ gatsby-config.js
@@ -39,6 +39,21 @@ module.exports = {
       resolve: `gatsby-transformer-remark`,
       options: {
         plugins: [
+                {
+            resolve: `gatsby-remark-images-grid`,
+                options: {
+                gridGap: `1rem`,
+            },
+        },
+        {
+          resolve: `gatsby-remark-images`,
+          options: {
+            // It's important to specify the maxWidth (in pixels) of
+            // the content container as this plugin uses this as the
+            // base for generating different widths of each image.
+            maxWidth: 960,
+          },
+        },
           {
             resolve: `gatsby-remark-prismjs`,
             options: {
@@ -55,7 +70,6 @@ module.exports = {
             },
           },
           `gatsby-remark-responsive-iframe`,
-          'gatsby-remark-figure',
         ],
       },
     },
