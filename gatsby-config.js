aed3970d9aec04361498c0729e41127c0291e241
Matthias Jordan <matthias.k.jordan@gmail.com>
2019-06-04 09:44:13 -0700

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -17,6 +17,15 @@ try {
   }
 }
 
+const {
+  NODE_ENV,
+  URL: NETLIFY_SITE_URL = 'https://iammatthias.com',
+  DEPLOY_PRIME_URL: NETLIFY_DEPLOY_URL = NETLIFY_SITE_URL,
+  CONTEXT: NETLIFY_ENV = NODE_ENV,
+} = process.env
+const isNetlifyProduction = NETLIFY_ENV === 'production'
+const siteUrl = isNetlifyProduction ? NETLIFY_SITE_URL : NETLIFY_DEPLOY_URL
+
 module.exports = {
   siteMetadata: {
     siteUrl: config.siteUrl,
