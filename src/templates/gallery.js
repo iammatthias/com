feae11b227d81f55f5ced763cd4864fbe6574804
Matthias Jordan <matthias.k.jordan@gmail.com>
2019-07-16 15:26:51 -0700

    Update gallery.js

--- src/templates/gallery.js
+++ src/templates/gallery.js
@@ -10,7 +10,7 @@ const GalleryTemplate = ({ data }) => {
   const subGalleries = data.contentfulExtendedGallery.galleries
   return (
     <>
-      <SEO title={gallery.title} image={gallery.shareImage} description={gallery.body.childMarkdownRemark} />
+      <SEO title={gallery.title} image={gallery.shareImage} />
       <ContentHead
         displayExcerpt
         title={gallery.title}
