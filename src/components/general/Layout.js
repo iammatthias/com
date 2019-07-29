f0a7dd0806d5d9a3682d0a4ce4aee0705af512b1
Matthias Jordan <me@iammatthias.com>
2019-07-28 18:38:58 -0700

    Black status bar

--- src/components/general/Layout.js
+++ src/components/general/Layout.js
@@ -22,7 +22,7 @@ const Layout = ({ children, ...props }) => {
           <meta name="apple-mobile-web-app-capable" content="yes" />
           <meta
             name="apple-mobile-web-app-status-bar-style"
-            content="black-translucent"
+            content="black"
           />
           <link
             rel="apple-touch-startup-image"
