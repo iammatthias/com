5a3fb440ae53eef1ee54f2e0afaf6ecb8c463110
Matthias Jordan <matthias.k.jordan@gmail.com>
2019-10-24 13:21:23 -0700

    MDX components

---
+++ src/components/mdx/index.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+import YouTube from 'react-youtube'
+
+import Title from './Title'
+import Paragraph from './Paragraph'
+
+import Link from './Link'
+
+export const MDXLayoutComponents = {
+  h1: props => <Title {...props} />,
+  h2: props => <Subtitle {...props} />,
+  p: props => <Paragraph {...props} />,
+}
+
+export const MDXGlobalComponents = {
+  Link,
+  YouTube,
+}
