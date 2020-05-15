7a1f60f4531409c62ae5ee45399123d2fc3a3ce0
Matthias Jordan <matthias.k.jordan@gmail.com>
2020-05-14 18:45:59 -0700

    add photo feed

--- gatsby-config.js
+++ gatsby-config.js
@@ -51,7 +51,7 @@ module.exports = {
     `,
         feeds: [
           {
-            name: 'feed',
+            name: 'feedPost',
             query: `
             {
               allContentfulPost(sort: { fields: [publishDate], order: DESC }, limit: 1) {
@@ -70,7 +70,33 @@ module.exports = {
               return allContentfulPost.edges.map(edge => {
                 return {
                   title: edge.node.title,
-                  url: site.siteMetadata.siteUrl + edge.node.slug,
+                  url: site.siteMetadata.siteUrl + '/' + edge.node.slug,
+                  date: edge.node.publishDate,
+                }
+              })
+            },
+          },
+          {
+            name: 'feedPhoto',
+            query: `
+            {
+              allContentfulPhotography(sort: { fields: [publishDate], order: DESC }, limit: 1) {
+                edges {
+                  node {
+                  title
+                  id
+                  slug
+                  publishDate(formatString: "MMMM DD, YYYY")
+                  }
+                }
+              }
+            }
+            `,
+            normalize: ({ query: { site, allContentfulPhotography } }) => {
+              return allContentfulPhotography.edges.map(edge => {
+                return {
+                  title: edge.node.title,
+                  url: site.siteMetadata.siteUrl + '/' + edge.node.slug,
                   date: edge.node.publishDate,
                 }
               })
