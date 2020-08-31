5f403e229e924b1eef0737a52d32f85fb26052f9
Matthias Jordan <matthias.k.jordan@gmail.com>
2020-08-30 21:50:13 -0700

    bad date field

--- gatsby-config.js
+++ gatsby-config.js
@@ -65,7 +65,7 @@ module.exports = {
                     internal {
                       type
                     }
-                    updatedAt(formatString: "MMMM DD, YYYY")
+                    publishDate(formatString: "MMMM DD, YYYY")
                   }
                 }
               }
