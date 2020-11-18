9054eeb773e1b0afac8cd0d94a7265af088a8d87
Matthias Jordan <matthias.k.jordan@gmail.com>
2020-11-17 17:50:13 -0800

    mdx

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -9,26 +9,16 @@ import { MDXRenderer } from 'gatsby-plugin-mdx'
 const Wrapper = styled.section`
   position: relative;
   width: calc(100% - 96px);
-
   max-width: ${props => props.theme.sizes.maxWidth};
   flex-grow: 1;
   margin: 16px auto;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
   @media screen and (min-width: 1281px) {
     width: calc(100% - 576px);
     margin: 64px auto;
-  }
-`
-
-const List = styled.ul`
-  display: flex;
-  flex-flow: row wrap;
-  justify-content: space-between;
-  align-items: flex-start;
-  width: 100%;
-  li {
-    margin-bottom: 16px;
-    @media screen and (min-width: 1281px) { 
-      margin-bottom: 0;
   }
 `
 
@@ -37,24 +27,21 @@ const Footer = () => {
 
   return (
     <Wrapper>
-      <List>
-        <Styled.li>
-          <MDXRenderer>{contentfulMeta.footer.childMdx.body}</MDXRenderer>
-        </Styled.li>
-        <Styled.li>
-          <a
-            href="https://www.contentful.com/"
-            rel="nofollow noopener noreferrer"
-            target="_blank"
-          >
-            <img
-              src="https://images.ctfassets.net/fo9twyrwpveg/44baP9Gtm8qE2Umm8CQwQk/c43325463d1cb5db2ef97fca0788ea55/PoweredByContentful_LightBackground.svg"
-              style={{ width: '100px' }}
-              alt="Powered by Contentful"
-            />
-          </a>
-        </Styled.li>
-      </List>
+      <div sx={{ mb: '10vh' }}>
+        <MDXRenderer>{contentfulMeta.footer.childMdx.body}</MDXRenderer>
+      </div>
+
+      <a
+        href="https://www.contentful.com/"
+        rel="nofollow noopener noreferrer"
+        target="_blank"
+      >
+        <img
+          src="https://images.ctfassets.net/fo9twyrwpveg/44baP9Gtm8qE2Umm8CQwQk/c43325463d1cb5db2ef97fca0788ea55/PoweredByContentful_LightBackground.svg"
+          style={{ width: '100px' }}
+          alt="Powered by Contentful"
+        />
+      </a>
     </Wrapper>
   )
 }
