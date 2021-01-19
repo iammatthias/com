444f40e6116e310b5eedacf5bd8c7a248529f067
Matthias Jordan <matthias.k.jordan@gmail.com>
2021-01-19 10:24:14 -0800

    HYDRATION

--- src/gatsby-plugin-theme-ui/index.js
+++ src/gatsby-plugin-theme-ui/index.js
@@ -1,5 +1,6 @@
 export default {
-  useColorSchemeMediaQuery: true,
+  useCustomProperties: true,
+  useColorSchemeMediaQuery: false,
   initialColorModeName: 'light',
   space: [0, 4, 8, 16, 32, 64, 128, 256, 512],
   fonts: {
