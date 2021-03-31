1be425f592c2b09432cb3e321a0bd3ade58e609d
Matthias Jordan <matthias.k.jordan@gmail.com>
2021-03-31 13:31:16 -0700

    oops

--- src/pages/guestbook.js
+++ src/pages/guestbook.js
@@ -13,21 +13,28 @@ import { useAuth } from '../hooks/use-auth';
 // markup
 
 export default function Guestbook() {
-  const { login, logout } = useAuth();
+  const { login, logout, currentUser } = useAuth();
+  const user = currentUser();
+  const userAddress = user?.get('ethAddress');
 
   return (
     <Layout wrapped>
       <Box sx={{ padding: ['1rem', '2rem', '4rem'] }}>
         {user ? (
-          <Button
-            onClick={() => {
-              return logout().catch((e) => {
-                console.error(e);
-              });
-            }}
-          >
-            Logout
-          </Button>
+          <>
+            ETH Address: {userAddress}
+            <br />
+            <br />
+            <Button
+              onClick={() => {
+                return logout().catch((e) => {
+                  console.error(e);
+                });
+              }}
+            >
+              Logout
+            </Button>
+          </>
         ) : (
           <Button
             onClick={() => {
