f4100429cb2a5a2461976487232798265d2b7b78
Matthias Jordan <matthias.k.jordan@gmail.com>
2021-04-01 14:36:57 -0700

    Remove master key

--- src/hooks/use-moralis.js
+++ src/hooks/use-moralis.js
@@ -4,10 +4,7 @@ export function useMoralis() {
   // Moralis Initialization
   if (typeof window !== `undefined`) {
     Moralis = require('moralis');
-    Moralis.initialize(
-      process.env.MORALIS_APPLICATION_ID,
-      process.env.MORALIS_MASTER_KEY
-    );
+    Moralis.initialize(process.env.MORALIS_APPLICATION_ID);
     Moralis.serverURL = 'https://1bmita1upxl9.moralis.io:2053/server';
   }
   return { Moralis };
