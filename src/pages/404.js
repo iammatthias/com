26d61b54e6232964c86ae20167a693c454fd1f90
iammatthias <me@iammatthias.com>
2021-07-11 14:12:57 -0700

    404

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,56 +1,80 @@
-import * as React from 'react';
-import { Link } from 'gatsby';
+/** @jsx jsx */
+import { jsx, Grid } from 'theme-ui';
+import React from 'react'; //eslint-disable-line
 
+import Layout from '../components/layout';
 import Seo from '../components/seo';
 
-// styles
-const pageStyles = {
-  color: '#232129',
-  padding: '96px',
-  fontFamily: '-apple-system, Roboto, sans-serif, serif',
-};
-const headingStyles = {
-  marginTop: 0,
-  marginBottom: 64,
-  maxWidth: 320,
-};
-
-const paragraphStyles = {
-  marginBottom: 48,
-};
-const codeStyles = {
-  color: '#8A6534',
-  padding: 4,
-  backgroundColor: '#FFF4DB',
-  fontSize: '1.25rem',
-  borderRadius: 4,
-};
-
 // markup
-const NotFoundPage = () => {
+const IndexPage = () => {
   return (
-    <main style={pageStyles}>
+    <Layout>
       <Seo />
-      <h1 style={headingStyles}>Page not found</h1>
-      <p style={paragraphStyles}>
-        Sorry{' '}
-        <span role="img" aria-label="Pensive emoji">
-          😔
-        </span>{' '}
-        we couldn’t find what you were looking for.
-        <br />
-        {process.env.NODE_ENV === 'development' ? (
-          <>
-            <br />
-            Try creating a page in <code style={codeStyles}>src/pages/</code>.
-            <br />
-          </>
-        ) : null}
-        <br />
-        <Link to="/">Go home</Link>.
-      </p>
-    </main>
+
+      <article sx={{ p: ['.5rem', '2rem'] }}>
+        <h1>404</h1>
+        <div
+          sx={{
+            width: ['100%', '50%'],
+            height: '0',
+            pb: ['42%', '21%'],
+            position: 'relative',
+          }}
+        >
+          <iframe
+            src="https://giphy.com/embed/2t9mb0HwTVn3mHj8AZ"
+            width="100%"
+            height="100%"
+            sx={{ position: 'absolute' }}
+            frameBorder="0"
+            className="giphy-embed"
+            allowFullScreen
+            title="gif1"
+          ></iframe>
+        </div>
+
+        <div
+          sx={{
+            width: ['100%', '50%'],
+            height: '0',
+            pb: ['100%', '50%'],
+            position: 'relative',
+          }}
+        >
+          <iframe
+            src="https://giphy.com/embed/1Zr9tBf3ZbPaUkjSnQ"
+            width="100%"
+            height="100%"
+            sx={{ position: 'absolute' }}
+            frameBorder="0"
+            className="giphy-embed"
+            allowFullScreen
+            title="gif2"
+          ></iframe>
+        </div>
+
+        <div
+          sx={{
+            width: ['100%', '50%'],
+            height: '0',
+            pb: ['100%', '50%'],
+            position: 'relative',
+          }}
+        >
+          <iframe
+            src="https://giphy.com/embed/YrMrSUfeh5do2FISt8"
+            width="100%"
+            height="100%"
+            sx={{ position: 'absolute' }}
+            frameBorder="0"
+            className="giphy-embed"
+            allowFullScreen
+            title="gif3"
+          ></iframe>
+        </div>
+      </article>
+    </Layout>
   );
 };
 
-export default NotFoundPage;
+export default IndexPage;
