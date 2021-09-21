4b62f8674391b566f2f513038d157e4e32dac014
Matthias Jordan <matthias.k.jordan@gmail.com>
2021-09-21 01:56:04 +0000

    progress: guestbook, cloudfunction still broken

--- components/guestbookList.js
+++ components/guestbookList.js
@@ -7,7 +7,7 @@ import { Box } from 'theme-ui'
 import Link from 'next/link'
 import Loading from './loading'
 
-export default function GuestbookList({ props }) {
+export default async function GuestbookList({ props }) {
   const { data, error, isLoading } = useMoralisCloudFunction('getGuestbook')
 
   if (isLoading) {
@@ -24,46 +24,48 @@ export default function GuestbookList({ props }) {
     return null
   }
 
-  const GuestList = data.map(guest => {
-    let signedAtDate = new Date(guest.updatedAt).toLocaleDateString('en-gb')
-    let signedAtTime = new Date(guest.updatedAt).toLocaleTimeString('en-US')
-    let address = guest.ethAddress
-    return (
-      <Box
-        key={address}
-        sx={{
-          bg: lighten('background', 0.025),
-          my: '1rem',
-          width: ['calc(100vw - 4rem)', '50%'],
-          p: '.5rem',
-          borderRadius: '4px',
-        }}
-      >
-        <small
-          sx={{ m: 0, p: '0 0 1rem', fontWeight: 'bold', fontFamily: 'mono' }}
-        >
-          {signedAtDate} at {signedAtTime}
-        </small>
-        <p
+  if (data) {
+    const GuestList = data.map(guest => {
+      let signedAtDate = new Date(guest.updatedAt).toLocaleDateString('en-gb')
+      let signedAtTime = new Date(guest.updatedAt).toLocaleTimeString('en-US')
+      let address = guest.ethAddress
+      return (
+        <Box
+          key={address}
           sx={{
-            m: 0,
-            p: '0',
-            overflow: 'scroll',
-            textOverflow: 'clip ellipsis',
-            fontFamily: 'mono',
+            bg: lighten('background', 0.025),
+            my: '1rem',
+            width: ['calc(100vw - 4rem)', '50%'],
+            p: '.5rem',
+            borderRadius: '4px',
           }}
         >
-          å
-          <Link
-            href={'https://etherscan.io/address/' + address}
-            sx={{ fontWeight: 'normal', textDecoration: 'none' }}
+          <small
+            sx={{ m: 0, p: '0 0 1rem', fontWeight: 'bold', fontFamily: 'mono' }}
           >
-            {address}
-          </Link>
-        </p>
-      </Box>
-    )
-  })
+            {signedAtDate} at {signedAtTime}
+          </small>
+          <p
+            sx={{
+              m: 0,
+              p: '0',
+              overflow: 'scroll',
+              textOverflow: 'clip ellipsis',
+              fontFamily: 'mono',
+            }}
+          >
+            å
+            <Link
+              href={'https://etherscan.io/address/' + address}
+              sx={{ fontWeight: 'normal', textDecoration: 'none' }}
+            >
+              {address}
+            </Link>
+          </p>
+        </Box>
+      )
+    })
 
-  return <>{GuestList}</>
+    return { GuestList }
+  }
 }
