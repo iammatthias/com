cede532ced93e09aee218e7a738ded0d42a77267
Matthias Jordan <matthias.k.jordan@gmail.com>
2021-09-21 01:20:21 +0000

    re-add guestbook

---
+++ components/guestbookList.js
@@ -0,0 +1,69 @@
+/** @jsxImportSource theme-ui */
+
+// guestbook list
+
+import { useMoralisCloudFunction } from 'react-moralis'
+import { Box } from 'theme-ui'
+import Link from 'next/link'
+import Loading from './loading'
+
+export default function GuestbookList({ props }) {
+  const { data, error, isLoading } = useMoralisCloudFunction('getGuestbook')
+
+  if (isLoading) {
+    return (
+      <>
+        <Loading />
+        <hr />
+      </>
+    )
+  }
+
+  if (error) {
+    console.error(error)
+    return null
+  }
+
+  const GuestList = data.map(guest => {
+    let signedAtDate = new Date(guest.updatedAt).toLocaleDateString('en-gb')
+    let signedAtTime = new Date(guest.updatedAt).toLocaleTimeString('en-US')
+    let address = guest.ethAddress
+    return (
+      <Box
+        key={address}
+        sx={{
+          bg: lighten('background', 0.025),
+          my: '1rem',
+          width: ['calc(100vw - 4rem)', '50%'],
+          p: '.5rem',
+          borderRadius: '4px',
+        }}
+      >
+        <small
+          sx={{ m: 0, p: '0 0 1rem', fontWeight: 'bold', fontFamily: 'mono' }}
+        >
+          {signedAtDate} at {signedAtTime}
+        </small>
+        <p
+          sx={{
+            m: 0,
+            p: '0',
+            overflow: 'scroll',
+            textOverflow: 'clip ellipsis',
+            fontFamily: 'mono',
+          }}
+        >
+          å
+          <Link
+            href={'https://etherscan.io/address/' + address}
+            sx={{ fontWeight: 'normal', textDecoration: 'none' }}
+          >
+            {address}
+          </Link>
+        </p>
+      </Box>
+    )
+  })
+
+  return <>{GuestList}</>
+}
--- pages/guestbook.js
+++ pages/guestbook.js
@@ -1,23 +1,32 @@
 /** @jsxImportSource theme-ui */
 
-import { MDXRemote } from 'next-mdx-remote'
 import { Box } from 'theme-ui'
+import { MoralisProvider } from 'react-moralis'
+import GuestbookCTA from '../components/guestbookCTA'
+import GuestbookList from '../components/guestbookList'
 
 export default function Home() {
   return (
-    <Box
-      sx={{
-        bg: 'background',
-        boxShadow: 'card',
-        borderRadius: '4px',
-        gridArea: 'body',
-      }}
+    <MoralisProvider
+      appId={process.env.NEXT_PUBLIC_MORALIS_APPLICATION_ID}
+      serverUrl={process.env.NEXT_PUBLIC_MORALIS_SERVER_ID}
     >
-      <Box sx={{ p: [3, 3, 4] }}>
-        <article>
-          <p>hello, guest</p>
-        </article>
+      <Box
+        sx={{
+          bg: 'background',
+          boxShadow: 'card',
+          borderRadius: '4px',
+          gridArea: 'body',
+        }}
+      >
+        <Box sx={{ p: [3, 3, 4] }}>
+          <article>
+            <GuestbookCTA />
+            <br />
+            <GuestbookList />
+          </article>
+        </Box>
       </Box>
-    </Box>
+    </MoralisProvider>
   )
 }
