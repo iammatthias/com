4ba73eaf2d542752a7566fb0c85adf0a89f353bc
iammatthias <me@iammatthias.com>
2021-09-25 21:24:42 -0700

    trailing slash

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 const withMDX = require('@next/mdx')({ extension: /\.mdx?$/ })
 module.exports = withMDX({
-  exportTrailingSlash: true,
+  trailingSlash: true,
   pageExtensions: ['js', 'mdx'],
   images: {
     domains: ['images.ctfassets.net'],
