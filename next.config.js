602bab8be3cc4dfc856248cb44cc407ffc05127c
iammatthias <me@iammatthias.com>
2021-10-29 10:15:24 -0700

    fix: add path redirect /photography/:slug -> /work/:slug

--- next.config.js
+++ next.config.js
@@ -6,4 +6,13 @@ module.exports = withMDX({
     domains: ['images.ctfassets.net'],
     formats: ['image/avif', 'image/webp'],
   },
+  async redirects() {
+    return [
+      {
+        source: '/photography/:slug',
+        destination: '/work/:slug', // Matched parameters can be used in the destination
+        permanent: true,
+      },
+    ]
+  },
 })
