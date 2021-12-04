5a90c95cd3ac1355b8e20aa704abfe3bb509cc91
iammatthias <me@iammatthias.com>
2021-12-03 22:33:19 -0800

    fix: masonry is the worst

--- components/gallery.js
+++ components/gallery.js
@@ -5,11 +5,10 @@
 import Image from 'next/image'
 import { useQuery, gql } from '@apollo/client'
 import { Box } from 'theme-ui'
-import { Masonry } from 'masonic'
+import Masonry from 'react-masonry-css'
 import { useRouter } from 'next/router'
 import useMeasure from 'react-use-measure'
 import Loading from './loading'
-import Squiggle from './squiggle'
 
 // lightbox
 import Lightbox from './galleryLightbox'
@@ -61,52 +60,62 @@ export default function Gallery(props) {
   const imageSetTitle = data.galleryCollection.items[0].title
   const imageSetImages = data.galleryCollection.items[0].imagesCollection.items
   const imageSetLength = imageSetImages.length
-  const columns = bounds.width / (imageSetLength >= 6 ? 6 : imageSetLength)
+  const columnLimit = 7
+  const columnWidth =
+    bounds.width /
+    (imageSetLength >= columnLimit ? columnLimit : imageSetLength)
+  const columns = imageSetLength >= columnLimit ? columnLimit : imageSetLength
 
-  // masonry
-
-  const MasonryCard = ({ index, data, width }) => (
-    <>
-      {/* <p>{(data.height / data.width) * columns}</p> */}
-      <div
-        key={index}
-        sx={{
-          position: 'relative',
-          width: '100%',
-          height: props.ratio
-            ? eval(props.ratio) * columns
-            : (data.height / data.width) * columns,
-          '*': {
-            borderRadius: '4px',
-            boxShadow: 'card',
-          },
-        }}
-      >
-        <Image
-          src={(length = 1 ? data.url : data.urlSmall)}
-          alt={data.title}
-          layout="fill"
-          placeholder="blur"
-          blurDataURL={data.loader}
-          objectFit="cover"
-          className="gallery"
-        />
-      </div>
-    </>
-  )
+  // bricks
 
   return (
     <Box ref={ref}>
       {pathname.includes('/work/') ? <h1>{imageSetTitle}</h1> : null}
 
-      <Lightbox>
+      <Lightbox className="container">
         <Masonry
-          items={imageSetImages}
-          render={MasonryCard}
-          columnGutter={16}
-          columnWidth={columns}
-          overscanBy={Infinity}
-        />
+          breakpointCols={columns}
+          className="my-masonry-grid"
+          sx={{
+            display: 'flex',
+            width: 'auto',
+            ml: '-8px',
+            '.column': {
+              pl: '8px',
+              mb: '8px',
+              backgroundClip: 'padding-box',
+            },
+          }}
+          columnClassName="column"
+        >
+          {imageSetImages.map((image, index) => (
+            <div
+              className="column"
+              key={index}
+              sx={{
+                position: 'relative',
+                width: '100%',
+                height: props.ratio
+                  ? eval(props.ratio) * columnWidth
+                  : (image.height / image.width) * columnWidth,
+                '*': {
+                  borderRadius: '4px',
+                  boxShadow: 'card',
+                },
+              }}
+            >
+              <Image
+                src={(length = 1 ? image.url : image.urlSmall)}
+                alt={image.title}
+                layout="fill"
+                placeholder="blur"
+                blurDataURL={image.loader}
+                objectFit="cover"
+                className="gallery"
+              />
+            </div>
+          ))}
+        </Masonry>
       </Lightbox>
     </Box>
   )
