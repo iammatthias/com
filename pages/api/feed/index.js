a89fe7724c22a365f1ff81a88c4f2a83273969ef
iammatthias <me@iammatthias.com>
2022-01-06 12:28:52 -0800

    update: feed content

--- pages/api/feed/index.js
+++ pages/api/feed/index.js
@@ -1,9 +1,10 @@
 export default function handler(req, res) {
-  res
-    .status(200)
-    .json({
-      name: 'Matthias Jordan',
-      handle: 'iammatthias',
-      work: 'Growth @ Tornado',
-    })
+  res.status(200).json({
+    name: 'Matthias Jordan',
+    email: 'hey@iammatthias.com',
+    handle: 'iammatthias',
+    company: 'Tornado',
+    twitter: 'twitter.com/',
+    git: 'github.com/',
+  })
 }
