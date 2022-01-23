7ef905ae2eb677e937854b1ad033d72d0813eb3c
iammatthias <me@iammatthias.com>
2022-01-23 14:02:49 -0800

    Improved color mode toggle

--- components/colorSwitcher.js
+++ components/colorSwitcher.js
@@ -1,5 +1,7 @@
 /** @jsxImportSource theme-ui */
 import { IconButton, useColorMode } from 'theme-ui'
+import SunIcon from './joy/sun'
+import MoonIcon from './joy/moon'
 
 const ColorSwitcher = props => {
   const [mode, setMode] = useColorMode()
@@ -10,32 +12,12 @@ const ColorSwitcher = props => {
       sx={{
         color: 'accent',
         cursor: 'pointer',
-        borderRadius: '50px',
-        mx: 3,
-        transition: 'box-shadow .125s ease-in-out',
-        ':hover,:focus': {
-          boxShadow: '0 0 0 2px',
-          outline: 'none',
-        },
+        fontSize: '16px',
       }}
       {...props}
     >
-      <svg
-        viewBox="0 0 32 32"
-        width={24}
-        height={24}
-        fill="none"
-        sx={{ borderRadius: '50px', boxShadow: 'card' }}
-      >
-        <circle
-          cx={16}
-          cy={16}
-          r={14}
-          stroke="currentcolor"
-          strokeWidth={3}
-          sx={{ fill: 'transparent' }}
-        />
-      </svg>
+      {mode === 'dark' && <MoonIcon />}
+      {mode === 'light' && <SunIcon />}
     </IconButton>
   )
 }
