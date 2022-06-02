1e59c251f7e9d3e754a68fdebef9cb5a1d23d4d5
iammatthias <me@iammatthias.com>
2022-06-02 11:22:12 -0700

    fix: typescale

--- src/lib/mdProvider.tsx
+++ src/lib/mdProvider.tsx
@@ -1,5 +1,10 @@
+import { Text } from '@/components/primitives/text';
 const mdComponents = {
-  p: (props: any) => <p {...props} style={{ fontSize: `14px` }} />,
+  // p: (props: any) => <p {...props} style={{ fontSize: `14px` }} />,
+  p: (props: any) => (
+    <Text as="small" {...props} style={{ display: `block` }} />
+  ),
+
   strong: (props: any) => <strong {...props} />,
   em: (props: any) => <em {...props} />,
   img: (props: any) => (
@@ -11,24 +16,12 @@ const mdComponents = {
       style={{ width: `50%` }}
     />
   ),
-  h1: (props: any) => (
-    <h1 {...props} className="mono" style={{ fontWeight: `bold` }} />
-  ),
-  h2: (props: any) => (
-    <h2 {...props} className="mono" style={{ fontWeight: `bold` }} />
-  ),
-  h3: (props: any) => (
-    <h3 {...props} className="mono" style={{ fontWeight: `bold` }} />
-  ),
-  h4: (props: any) => (
-    <h4 {...props} className="mono" style={{ fontWeight: `bold` }} />
-  ),
-  h5: (props: any) => (
-    <h5 {...props} className="mono" style={{ fontWeight: `bold` }} />
-  ),
-  h6: (props: any) => (
-    <h6 {...props} className="mono" style={{ fontWeight: `bold` }} />
-  ),
+  h1: (props: any) => <Text as="h1" {...props} />,
+  h2: (props: any) => <Text as="h2" {...props} />,
+  h3: (props: any) => <Text as="h3" {...props} />,
+  h4: (props: any) => <Text as="h4" {...props} />,
+  h5: (props: any) => <Text as="h5" {...props} />,
+  h6: (props: any) => <Text as="h6" {...props} />,
   figure: (props: any) => <figure {...props} style={{ fontSize: `14px` }} />,
 };
 
