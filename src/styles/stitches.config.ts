2cec46a6ee4b146dffa55ffe711f7b488dce7cbc
iammatthias <me@iammatthias.com>
2022-06-02 18:19:35 -0700

    fix: adjust bg fade

--- src/styles/stitches.config.ts
+++ src/styles/stitches.config.ts
@@ -165,7 +165,7 @@ export const globalStyles = globalCss({
     fontSize: `1.125em`,
     lineHeight: `1.6`,
     padding: `2rem 1rem 4rem`,
-    background: `radial-gradient(circle at 50% 50%, $background, rgba(255,255,255,0)),
+    background: `radial-gradient(circle at 50% 50%, $background, $faded),
 url("data:image/svg+xml,%3Csvg viewBox='0 0 400 400' xmlns='http://www.w3.org/2000/svg'%3E%3Cfilter id='noiseFilter'%3E%3CfeTurbulence type='fractalNoise' baseFrequency='4.67' numOctaves='3' stitchTiles='stitch'/%3E%3C/filter%3E%3Crect width='100%25' height='100%25' filter='url(%23noiseFilter)'/%3E%3C/svg%3E")`,
     backgroundSize: `cover`,
   },
