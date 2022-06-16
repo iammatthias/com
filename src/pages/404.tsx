34fa49e7f7208307927323a0006b7bd35c2882ed
iammatthias <me@iammatthias.com>
2022-06-16 11:21:30 -0700

    fix: move variable font header to its own components so it can be used outside of page headers

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -2,7 +2,7 @@
 
 import Link from 'next/link';
 import { Button } from '@/components/primitives/button';
-import { Text } from '@/components/primitives/text';
+import Text from '@/components/primitives/text';
 
 // components
 
