3d5086ac48fefcd987c7b99fdd5fb77a55760ab1
iammatthias <me@iammatthias.com>
2022-06-28 18:39:20 -0700

    fix: auto update timestamp

--- src/components/Navigation/index.tsx
+++ src/components/Navigation/index.tsx
@@ -1,22 +1,24 @@
-import React from 'react';
 import Image from 'next/image';
 import { navigationRecipe } from './Navigation.css';
 import Box from '@/components/Box';
 import Text from '@/components/Text';
 import Icon from '../../../public/images/navBarIcon.png';
 import NavigationMenu from './NavigationMenu';
+import Timestamp from '@/utils/timestamp';
 
 export const Navigation = () => {
-  const timeStamp = new Date().toLocaleTimeString([], {
-    hour: `2-digit`,
-    minute: `2-digit`,
-  });
+  // const timeStamp = new Date().toLocaleTimeString([], {
+  //   hour: `2-digit`,
+  //   minute: `2-digit`,
+  // });
   return (
     <>
       <Box as="section" className={navigationRecipe({ nav: `main` })}>
         <Image src={Icon} width={15} height={15} />
 
-        <Text kind="nav">{timeStamp}</Text>
+        <Text kind="nav">
+          <Timestamp />
+        </Text>
       </Box>
       <NavigationMenu />
     </>
---
+++ src/utils/timestamp.tsx
@@ -0,0 +1,24 @@
+import { useState, useEffect } from 'react';
+
+export const Timestamp = () => {
+  const [date, setDate] = useState(new Date());
+
+  useEffect(() => {
+    const timer = setInterval(() => setDate(new Date()), 1000);
+    return function cleanup() {
+      clearInterval(timer);
+    };
+  });
+
+  return (
+    <>
+      {date.toLocaleTimeString([], {
+        hour: `2-digit`,
+        minute: `2-digit`,
+        second: `2-digit`,
+      })}
+    </>
+  );
+};
+
+export default Timestamp;
