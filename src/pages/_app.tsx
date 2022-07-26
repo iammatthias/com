e4998696424b4d10b2838042f2f52028b286009e
iammatthias <me@iammatthias.com>
2022-07-26 10:08:29 -0700

    fix: re-add SEO component

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,7 @@ import { AppProps } from 'next/app';
 
 import '@/styles/reset.css';
 
+import SEO from '@/components/seo';
 import Layout from '@/components/layout';
 import Background from '@/components/background';
 import MDXProvider from '@/utils/MDXprovider';
@@ -12,6 +13,7 @@ export default function MyApp({ Component, pageProps }: AppProps) {
     <Web3Provider>
       <MDXProvider>
         <Layout>
+          <SEO title={pageProps.pageTitle} name={pageProps.pageName} />
           <Component {...pageProps} />
         </Layout>
         <Background />
