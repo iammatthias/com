684d8d2eb02f2386fb430b3e1ae84ea44852827c
iammatthias <me@iammatthias.com>
2022-09-13 16:59:50 -0700

    fix: case reset

--- src/components/footer/footer.css.ts
+++
@@ -1,7 +0,0 @@
-import { style } from '@vanilla-extract/css';
-
-export const lowPolySVG = style({
-  height: `44px`,
-  margin: `64px auto`,
-  fill: `#131315`,
-});
--- src/components/subGrid/subgrid.css.ts
+++
@@ -1,8 +0,0 @@
-import { style } from '@vanilla-extract/css';
-
-export const subgrid = style({
-  position: `relative`,
-  display: `flex`,
-  flexDirection: `column`,
-  gap: `32px`,
-});
