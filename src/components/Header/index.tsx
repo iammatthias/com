eff493ea309a139b2442ca55d9e1cd8a707288bc
iammatthias <me@iammatthias.com>
2022-09-13 17:16:49 -0700

    fix: show time only on client

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -9,10 +9,12 @@ import {
 import Box from '@/components/Box';
 import Text from '@/components/Text';
 import Time from './time';
+import useIsClient from '@/utils/useIsClient';
 
 export default function Header() {
   const router = useRouter();
   const { pathname } = router;
+  const isClient = useIsClient();
   return (
     <Box className={`${header} ${headerStart}`}>
       <Box className={`${headerGrid}`}>
@@ -28,7 +30,7 @@ export default function Header() {
         </Box>
         <Box className={`${headerEnd}`}>
           <Text as="p" kind="small" navBar={true}>
-            <Time />
+            {isClient && <Time />}
           </Text>
         </Box>
       </Box>
