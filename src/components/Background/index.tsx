c328c2bc02bf1e409e209c5091c7495dae50a445
iammatthias <me@iammatthias.com>
2022-09-14 11:24:48 -0700

    fix: case issues

--- src/components/Background/index.tsx
+++
@@ -1,9 +0,0 @@
-import Box from '@/components/box';
-
-import { background } from './background.css';
-
-export const Background = () => {
-  return <Box as="section" className={`${background}`} />;
-};
-
-export default Background;
--- src/utils/DateFormat.js
+++
@@ -1,6 +0,0 @@
-export default function dateFormat(date) {
-  const _date = new Date(
-    date.replace(/-/g, `/`).replace(/T.+/, ``),
-  ).toLocaleDateString(`en-us`);
-  return _date;
-}
