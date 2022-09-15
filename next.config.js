420f7810c583d8fe512e5154491d7b1aa5971b70
iammatthias <me@iammatthias.com>
2022-09-15 15:16:08 -0700

    fix: pwa

--- next.config.js
+++ next.config.js
@@ -1,13 +1,33 @@
 const { createVanillaExtractPlugin } = require('@vanilla-extract/next-plugin');
 const withVanillaExtract = createVanillaExtractPlugin();
+const withPWA = require('next-pwa');
 
 /** @type {import('next').NextConfig} */
+/** @type {import('next-sitemap').IConfig} */
+
 const nextConfig = {
+  generateRobotsTxt: true,
   reactStrictMode: true,
   swcMinify: true,
   images: {
     domains: ['pbs.twimg.com'],
   },
+  pwa: {
+    dest: 'public',
+  },
+  async headers() {
+    return [
+      {
+        source: '/fonts/:font*',
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'public, immutable, max-age=31536000',
+          },
+        ],
+      },
+    ];
+  },
 };
 
-module.exports = withVanillaExtract(nextConfig);
+module.exports = withVanillaExtract(withPWA(nextConfig));
