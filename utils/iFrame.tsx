5cfc102693599447a72c8a2c5013b89744e790b9
iammatthias <me@iammatthias.com>
2022-11-13 15:47:39 -0800

    fix: remove id from iframe resizer so it works for multiple iframes

--- utils/iFrame.tsx
+++ utils/iFrame.tsx
@@ -6,12 +6,15 @@ import IframeResizer from 'iframe-resizer-react';
 
 export default function IFrame(props: any) {
   const iframeRef = useRef(null);
+
   return (
     <IframeResizer
       forwardRef={iframeRef}
+      checkOrigin={false}
+      inPageLinks
+      log={false}
       src={props.src}
       frameBorder="0"
-      id="embed"
       heightCalculationMethod="taggedElement"
     />
   );
