473039b3120ad4e601b0203574821ed090222262
iammatthias <me@iammatthias.com>
2022-11-29 11:16:03 -0800

    fix: font loading

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
 import Link from 'next/link';
-import { Crimson_Pro } from '@next/font/google';
+import { Crimson_Pro, Space_Grotesk } from '@next/font/google';
 
 import DateTime from './components/dateTime';
 // import Relay from './components/relay';
@@ -9,10 +9,8 @@ import './global.css';
 import layout from './layout.module.css';
 import Head from './head';
 
-const crimson_pro = Crimson_Pro({ subsets: [`latin`] });
-
-// const date = new Date().toLocaleDateString();
-// const time = new Date().toLocaleTimeString('en-US', { hour12: false });
+const crimson_pro = Crimson_Pro();
+const space_grotesk = Space_Grotesk();
 
 export default function RootLayout({
   children,
@@ -23,7 +21,7 @@ export default function RootLayout({
     <html lang="en">
       <Head />
       <body className={`${layout.layout} ${crimson_pro.className}`}>
-        <header className={layout.header}>
+        <header className={`${layout.header} `}>
           <p>
             <Link href="/" title="hey, I am Matthias">
               Hey, I am Matthias
@@ -34,7 +32,7 @@ export default function RootLayout({
 
         <main className={layout.main}>{children}</main>
         {/* <Relay /> */}
-        <footer className={layout.footer}>
+        <footer className={`${layout.footer} `}>
           <p>
             <Link href="mailto:hey@iammatthias.com?subject=Hello%20there!">
               hey@iammatthias.com
--- next.config.js
+++ next.config.js
@@ -2,6 +2,9 @@
 const nextConfig = {
   experimental: {
     appDir: true,
+    fontLoaders: [
+      { loader: '@next/font/google', options: { subsets: ['latin'] } },
+    ],
   },
   images: {
     domains: ['pub-8bcf4a42832e4273a5a34c696ccc1b55.r2.dev'],
