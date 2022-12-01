1872d7d6efe50aea981a33f9e2af43fcc2e21656
iammatthias <me@iammatthias.com>
2022-12-01 08:21:23 -0800

    fix: merge conflicts

--- next.config.js
+++ next.config.js
@@ -1,18 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-<<<<<<< HEAD
   experimental: {
     appDir: true,
     fontLoaders: [{ loader: '@next/font/google', options: { subsets: ['latin'] } }],
   },
   images: {
     domains: ['pub-8bcf4a42832e4273a5a34c696ccc1b55.r2.dev'],
-=======
-  reactStrictMode: true,
-  swcMinify: true,
-  experimental: {
-    appDir: true,
->>>>>>> main
   },
 };
 
