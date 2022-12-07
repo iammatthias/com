491086d75b4ac7facd747e85fa80b327671520bf
iammatthias <me@iammatthias.com>
2022-12-06 19:33:27 -0800

    fix: hid mint ui in Zora iframes

--- utils/markdown.tsx
+++ utils/markdown.tsx
@@ -3,30 +3,8 @@ import imageSize from 'image-size';
 import axios from 'axios';
 import IFrame from './iFrame';
 
-// function Image(props: any) {
-//   const uri = `https://pub-8bcf4a42832e4273a5a34c696ccc1b55.r2.dev/${props.src}`;
-
-//   const image = axios.get(uri, { responseType: 'arraybuffer' }).then((response) => {
-//     const dimensions = imageSize(response.data);
-//     return (
-//       <figure style={{ position: `relative` }}>
-//         <NextImage
-//           src={uri}
-//           alt={props.alt}
-//           width={dimensions.width}
-//           height={dimensions.height}
-//           style={{ objectFit: `contain`, height: `fit-content`, width: `100%` }}
-//         />
-//         {props.alt && <figcaption>{props.alt}</figcaption>}
-//       </figure>
-//     );
-//   });
-
-//   return image;
-// }
-
 function Frame(props: any) {
-  const uri = `${props.src}/frame?padding=20px&showDetails=false&theme=light&showPresale=false&showMedia=true&showCollectors=false`;
+  const uri = `${props.src}/frame?padding=0px&mediaPadding=20px&showDetails=false&theme=light&showMedia=true&showCollectors=false&showMintingUI=false`;
 
   return <IFrame src={uri} _src={props.src} />;
 }
