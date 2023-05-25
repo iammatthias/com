e2d88b919de1a58dff69dc38fa4420244020c3e7
iammatthias <me@iammatthias.com>
2023-05-24 21:15:47 -0700

    Working on an edge function to store bookmark casts in supabase

--- src/app/api/farcaster-bookmarks/route.ts
+++ src/app/api/farcaster-bookmarks/route.ts
@@ -3,7 +3,7 @@ import { createClient } from "@supabase/supabase-js";
 
 // Create a Supabase client
 const SUPABASE_URL = process.env.NEXT_PUBLIC_SUPABASE_URL as string;
-const SUPABASE_KEY = process.env.NEXT_PUBLIC_SUPABASE_KEY as string;
+const SUPABASE_KEY = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY as string;
 const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
 
 export async function GET() {
