2d13fa852b42407404c5e7b4484c22379b4e8542
iammatthias <me@iammatthias.com>
2023-05-24 21:01:40 -0700

    Fix some build errors

--- src/app/components/mdx/remoteImage.tsx
+++ src/app/components/mdx/remoteImage.tsx
@@ -12,12 +12,13 @@ function useAboveTheFold(): [React.RefObject<HTMLElement>, boolean] {
     const observer = new IntersectionObserver(([entry]) => {
       setIsInView(entry.isIntersecting);
     });
-    if (ref.current) {
-      observer.observe(ref.current);
+    const currentRef = ref.current;
+    if (currentRef) {
+      observer.observe(currentRef);
     }
     return () => {
-      if (ref.current) {
-        observer.unobserve(ref.current);
+      if (currentRef) {
+        observer.unobserve(currentRef);
       }
     };
   }, []);
