002f1482a69df27e3ffcdb0d303babe14f07f88b
iammatthias <me@iammatthias.com>
2023-05-29 18:24:03 -0700

    add metadata base so vercel stops compaining

--- src/app/api/farcaster-bookmarks/route.ts
+++ src/app/api/farcaster-bookmarks/route.ts
@@ -1,6 +1,8 @@
 import { NextResponse } from "next/server";
 import { createClient } from "@supabase/supabase-js";
 
+export const runtime = "edge";
+
 // Create a Supabase client
 const SUPABASE_URL = process.env.NEXT_PUBLIC_SUPABASE_URL as string;
 const SUPABASE_KEY = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY as string;
