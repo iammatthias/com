0ca6dc063d99f08d80c19780ec457e5fb7998016
iammatthias <me@iammatthias.com>
2023-05-30 06:45:53 -0700

    disable rough eventing with Umami

--- src/app/lib/trackers.tsx
+++ src/app/lib/trackers.tsx
@@ -1,15 +1,15 @@
-"use client";
+// "use client";
 
-import { useEffect } from "react";
+// import { useEffect } from "react";
 
-// define umami event tracker functions
-// https://umami.is/docs/tracker-functions
-// umami.track(event_name, [event_data])
+// // define umami event tracker functions
+// // https://umami.is/docs/tracker-functions
+// // umami.track(event_name, [event_data])
 
-export function trackEvent(event_name: string, event_data: any) {
-  if (typeof window.umami !== "undefined") {
-    useEffect(() => {
-      window.umami.track(event_name, event_data);
-    }, [event_name, event_data]);
-  }
-}
+// export function trackEvent(event_name: string, event_data: any) {
+//   if (typeof window.umami !== "undefined") {
+//     useEffect(() => {
+//       window.umami.track(event_name, event_data);
+//     }, [event_name, event_data]);
+//   }
+// }
