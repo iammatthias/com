122345a109110608ee24e6804bd79d73d7dbd011
iammatthias <me@iammatthias.com>
2023-11-02 20:20:04 -0700

    Tweek the onchain component to improve performance a bit

--- src/components/onchain/index.tsx
+++ src/components/onchain/index.tsx
@@ -1,32 +1,70 @@
 import Link from "next/link";
 import RemoteImage from "../remote_image";
 
-// Decode base64 data
-function decodeBase64Json(base64Str: string) {
-  const jsonStr = Buffer.from(base64Str, "base64").toString();
-  return JSON.parse(jsonStr);
-}
-
-async function fetchContractURI(address: string) {
-  const response = await fetch(`https://explorer.zora.energy/api/v2/smart-contracts/${address}/query-read-method`, {
+// Helper function to make API requests
+async function makeApiRequest(url: string, body: any) {
+  const response = await fetch(url, {
     method: "POST",
     headers: {
       accept: "application/json",
       "Content-Type": "application/json",
     },
-    body: JSON.stringify({
-      args: [],
-      method_id: "e8a3d485",
-      from: address,
-      contract_type: "proxy",
-    }),
+    body: JSON.stringify(body),
   });
 
   if (!response.ok) {
     throw new Error(`HTTP error! status: ${response.status}`);
   }
 
-  const data = await response.json();
+  return response.json();
+}
+
+// Decode base64 data
+function decodeBase64Json(base64Str: string) {
+  const jsonStr = Buffer.from(base64Str, "base64").toString();
+  return JSON.parse(jsonStr);
+}
+
+// Fetch functions use the helper to reduce repeated code
+async function fetchContractURI(address: string) {
+  const data = await makeApiRequest(
+    `https://explorer.zora.energy/api/v2/smart-contracts/${address}/query-read-method`,
+    {
+      args: [],
+      method_id: "e8a3d485",
+      from: address,
+      contract_type: "proxy",
+    }
+  );
+
+  return data.result.output[0].value.replace("ipfs://", "");
+}
+
+async function fetchNextTokenID(address: string) {
+  const data = await makeApiRequest(
+    `https://explorer.zora.energy/api/v2/smart-contracts/${address}/query-read-method`,
+    {
+      args: [],
+      method_id: "75794a3c",
+      from: address,
+      contract_type: "proxy",
+    }
+  );
+
+  return parseInt(data.result.output[0].value);
+}
+
+async function fetchTokenData(address: string, tokenId: number) {
+  const data = await makeApiRequest(
+    `https://explorer.zora.energy/api/v2/smart-contracts/${address}/query-read-method`,
+    {
+      args: [tokenId.toString()],
+      method_id: "0e89341c",
+      from: address,
+      contract_type: "proxy",
+    }
+  );
+
   return data.result.output[0].value.replace("ipfs://", "");
 }
 
@@ -35,8 +73,7 @@ async function fetchMetadata(hash: string) {
   if (hash.startsWith("data:")) {
     // Extract the base64 part and decode it
     const base64Data = hash.split(",")[1];
-    const jsonStr = atob(base64Data);
-    return JSON.parse(jsonStr);
+    return decodeBase64Json(base64Data);
   } else {
     // It's an IPFS hash, proceed with existing logic
     const gateways = [
@@ -61,98 +98,17 @@ async function fetchMetadata(hash: string) {
   throw new Error(`All IPFS gateways failed for hash: ${hash}`);
 }
 
-async function fetchNextTokenID(address: string) {
-  const response = await fetch(`https://explorer.zora.energy/api/v2/smart-contracts/${address}/query-read-method`, {
-    method: "POST",
-    headers: {
-      accept: "application/json",
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({
-      args: [],
-      method_id: "75794a3c",
-      from: address,
-      contract_type: "proxy",
-    }),
-  });
-
-  if (!response.ok) {
-    throw new Error(`HTTP error! status: ${response.status}`);
-  }
-
-  const data = await response.json();
-  return parseInt(data.result.output[0].value);
-}
-
-async function fetchTokenData(address: string, tokenId: number) {
-  const response = await fetch(`https://explorer.zora.energy/api/v2/smart-contracts/${address}/query-read-method`, {
-    method: "POST",
-    headers: {
-      accept: "application/json",
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({
-      args: [tokenId.toString()],
-      method_id: "0e89341c",
-      contract_type: "proxy",
-    }),
-  });
-
-  if (!response.ok) {
-    throw new Error(`HTTP error! status: ${response.status}`);
-  }
-
-  const data = await response.json();
-  return data.result.output[0].value.replace("ipfs://", "");
-}
-
+// Optimized function to fetch all token metadata in parallel
 export async function fetchAllTokenMetadata(address: string, nextTokenID: number) {
-  const tokenMetadataArray = [];
-  for (let i = 1; i < nextTokenID; i++) {
-    const tokenURIHash = await fetchTokenData(address, i);
-    const tokenMetadata = await fetchMetadata(tokenURIHash);
-    console.log(tokenMetadata);
-    tokenMetadataArray.push(tokenMetadata);
-  }
-  return tokenMetadataArray;
+  const promises = Array.from({ length: nextTokenID - 1 }, (_, i) =>
+    fetchTokenData(address, i + 1)
+      .then(fetchMetadata)
+      .catch((error) => console.error(`Token ${i + 1} fetch error:`, error))
+  );
+
+  const results = await Promise.allSettled(promises);
+  return results.filter((r) => r.status === "fulfilled").map((r: any) => r.value);
 }
-
-// export default async function Onchain({ address }: { address: string }) {
-//   try {
-//     const contractURI = await fetchContractURI(address);
-//     console.log("contractURI", contractURI);
-//     const contractMetadata = await fetchMetadata(contractURI);
-//     // console.log(contractMetadata);
-//     const contractMetadataDescription = contractMetadata.description;
-//     const nextTokenID = await fetchNextTokenID(address);
-
-//     const tokenMetadataArray = await fetchAllTokenMetadata(address, nextTokenID);
-
-//     return (
-//       <>
-//         <p>{contractMetadataDescription}</p>
-//         {tokenMetadataArray.map((tokenMetadata, i) => (
-//           <>
-//             <Link href={`https://zora.co/collect/zora:${address}/${i + 1}`}>
-//               <RemoteImage
-//                 src={tokenMetadata.image.replace("ipfs://", "https://ipfs.io/ipfs/")}
-//                 alt={tokenMetadata.name}
-//               />
-//             </Link>
-//             <p>{tokenMetadata.description}</p>
-//           </>
-//         ))}
-//       </>
-//     );
-//   } catch (error) {
-//     console.error("Error:", error);
-//     return (
-//       <>
-//         <p>Error fetching data for address: {address}</p>
-//       </>
-//     );
-//   }
-// }
 
 export default async function Onchain({ address }: { address: string }) {
   try {
@@ -164,7 +120,6 @@ export default async function Onchain({ address }: { address: string }) {
       // URI is base64 JSON data
       const base64Data = contractURI.split(",")[1];
       contractMetadata = decodeBase64Json(base64Data);
-      console.log(contractMetadata);
       return (
         <>
           <Link href={`https://zora.co/collect/zora:${address}`}>
@@ -199,9 +154,6 @@ export default async function Onchain({ address }: { address: string }) {
         </>
       );
     }
-
-    // Render or return the contract and token metadata
-    // The rest of your component logic will go here...
   } catch (error) {
     console.error("Error:", error);
     return (
