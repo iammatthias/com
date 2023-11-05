2bba4b6a4b1fcbf7a371bb3d8a1e6e6e8cfb1db3
iammatthias <me@iammatthias.com>
2023-11-04 20:17:08 -0700

    force cache?

--- src/lib/getPlaceholder.ts
+++ src/lib/getPlaceholder.ts
@@ -1,7 +1,7 @@
 import { getPlaiceholder } from "plaiceholder";
 
 export async function getPlaceholder(src: string) {
-  const buffer = await fetch(src).then(async (res) => Buffer.from(await res.arrayBuffer()));
+  const buffer = await fetch(src, { cache: "force-cache" }).then(async (res) => Buffer.from(await res.arrayBuffer()));
   const plaiceholderData = await getPlaiceholder(buffer);
   return {
     base64: plaiceholderData.base64,
