e9492447a53e9685908a8cdf17c6a84620f0a929
iammatthias <me@iammatthias.com>
2023-11-04 19:24:12 -0700

    Add a redis store for the base64 image blur placeholders

--- src/components/remote_image/index.tsx
+++ src/components/remote_image/index.tsx
@@ -1,33 +1,50 @@
 import Image from "next/image";
 import { getPlaiceholder } from "plaiceholder";
-import { Suspense } from "react";
+import { redisClient } from "@/lib/redis-client";
 
 export default async function RemoteImage({ src, alt, className }: { src: string; alt: string; className?: string }) {
-  //  if source is just a filename we need to add the url and fetch it from the Cloudflare bucket
+  // Adjust the source URL if needed
   src = src.includes(`http`) ? src : `https://pub-8bcf4a42832e4273a5a34c696ccc1b55.r2.dev/${src}`;
 
-  const buffer = await fetch(src).then(async (res) => {
-    return Buffer.from(await res.arrayBuffer());
-  });
+  // Define cache key
+  const cacheKey = `image-base64:${src}`;
 
-  const { base64, metadata } = await getPlaiceholder(buffer);
+  // Try to get cached base64 and metadata
+  let base64, metadata;
+  const cachedData = await redisClient.get(cacheKey);
 
-  const wsrv = `https://wsrv.nl/?w=900&dpr=2&n=-1&url=${src}`;
+  if (cachedData && typeof cachedData === "string") {
+    try {
+      const parsedData = JSON.parse(cachedData);
+      base64 = parsedData.base64;
+      metadata = parsedData.metadata;
+    } catch (error) {
+      console.error("Error parsing cachedData:", error);
+    }
+  } else {
+    // Fetch image buffer if not cached
+    const buffer = await fetch(src).then(async (res) => Buffer.from(await res.arrayBuffer()));
+    const plaiceholderData = await getPlaiceholder(buffer);
+    base64 = plaiceholderData.base64;
+    metadata = plaiceholderData.metadata;
 
-  const imageSrc = src.includes(`imgur`) || src.includes(`cdn.glass.photo`) ? src : wsrv;
+    // Cache the base64 and metadata
+    await redisClient.set(cacheKey, JSON.stringify({ base64, metadata }), { ex: 60 * 60 * 24 }); // Expires in 24 hours Expires in 24 hours
+  }
+
+  const imageSrc =
+    src.includes(`imgur`) || src.includes(`cdn.glass.photo`) ? src : `https://wsrv.nl/?w=900&dpr=2&n=-1&url=${src}`;
 
   return (
-    <Suspense>
-      <Image
-        className={className}
-        src={imageSrc}
-        alt={alt}
-        height={metadata.height}
-        width={metadata.width}
-        placeholder='blur'
-        blurDataURL={base64}
-        priority={false}
-      />
-    </Suspense>
+    <Image
+      className={className}
+      src={imageSrc}
+      alt={alt}
+      height={metadata.height}
+      width={metadata.width}
+      placeholder='blur'
+      blurDataURL={base64}
+      priority={false}
+    />
   );
 }
