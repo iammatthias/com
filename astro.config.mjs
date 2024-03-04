ba7e6ba6eedf94df3338e50ecee5851abe427ee5
iammatthias <me@iammatthias.com>
2024-03-04 15:36:10 -0800

    tweak vercel.json

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,7 @@ import vercel from "@astrojs/vercel/serverless";
 // https://astro.build/config
 export default defineConfig({
   integrations: [alpine()],
+
   output: "server",
   adapter: vercel({
     isr: true,
