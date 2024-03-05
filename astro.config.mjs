0fda64ce5ee9ab65ff94fb9b4aef947727219ac4
iammatthias <me@iammatthias.com>
2024-03-04 16:59:00 -0800

    Update Astro configuration and page prerendering

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,5 @@ import vercel from "@astrojs/vercel/serverless";
 export default defineConfig({
   integrations: [alpine()],
   output: "server",
-  adapter: vercel({
-    isr: true,
-  }),
+  adapter: vercel(),
 });
