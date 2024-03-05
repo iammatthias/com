abf97169ead752d071838abe00a38ef82d982c10
iammatthias <me@iammatthias.com>
2024-03-04 21:23:24 -0800

    Update Astro configuration and remove unused files

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,5 +7,7 @@ import vercel from "@astrojs/vercel/serverless";
 export default defineConfig({
   integrations: [alpine()],
   output: "server",
-  adapter: vercel(),
+  adapter: vercel({
+    isr: true,
+  }),
 });
