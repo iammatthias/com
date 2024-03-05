d6cede33f2ccdcbd5d6511ad2e40809c4abf7ed6
iammatthias <me@iammatthias.com>
2024-03-04 19:29:21 -0800

    remove ssr for now

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,6 @@ import vercel from "@astrojs/vercel/serverless";
 // https://astro.build/config
 export default defineConfig({
   integrations: [alpine()],
-  output: "server",
-  adapter: vercel(),
+  // output: "server",
+  // adapter: vercel(),
 });
