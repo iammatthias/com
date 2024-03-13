ecd47735cc5c7bdcbad18e6207a652cd9c63bc71
iammatthias <me@iammatthias.com>
2024-03-13 13:53:39 -0700

    Add Astro DB integration and update dependencies

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,18 +1,19 @@
 import { defineConfig } from "astro/config";
 import alpine from "@astrojs/alpinejs";
-
 import vercel from "@astrojs/vercel/serverless";
+
+import db from "@astrojs/db";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [alpine()],
+  integrations: [alpine(), db()],
   output: "server",
   adapter: vercel({
     isr: {
       // A secret random string that you create.
       // bypassToken: "005556d774a8",
       // expiration: 60 * 60 * 24,
-      expiration: 60,
-    },
-  }),
+      expiration: 60
+    }
+  })
 });
--- src/lib/github.ts
+++ src/lib/github.ts
@@ -1,20 +1,80 @@
 import matter from "gray-matter";
-import { parseAndMergeTags } from "./tags";
+// import { parseAndMergeTags } from "@lib/tags";
+
+import { db, eq, and, Posts, Tags, PostTags } from "astro:db";
+import crypto from "crypto";
 
 const github = import.meta.env.github;
+
+interface Frontmatter {
+  title: string;
+  slug: string;
+  published: boolean;
+  created: string;
+  updated: string;
+  tags: string[];
+  path: string;
+}
 
 // Updated parseMarkdownContent to incorporate tag, slug, and path extraction
 async function parseMarkdownContent(content: string, path: string) {
   const { data, content: body } = matter(content);
 
   // Add the path to the frontmatter
-  const frontmatter = { ...data, path };
+  // Add the path to the frontmatter
+  const frontmatter: Frontmatter = {
+    title: data.title || "",
+    slug: data.slug || "",
+    published: data.published || false,
+    created: data.created || "",
+    updated: data.updated || "",
+    tags: data.tags || [],
+    path,
+  };
 
   return { frontmatter, body };
 }
 
 // Helper function to fetch data from GitHub GraphQL API
-async function fetchFromGitHubGraphQL(query: string, variables: any) {
+async function fetchFromGitHubGraphQL(path?: string, slug?: string) {
+  // Adjust the expression based on whether a path and/or a slug is provided
+  let expression = "HEAD:content";
+  if (path && slug) {
+    expression += `/${path}/${slug}.md`;
+  } else if (path) {
+    expression += `/${path}`;
+  } else if (slug) {
+    expression += `/${slug}.md`;
+  }
+
+  const query = `
+    query fetchEntries($owner: String!, $name: String!, $expression: String!) {
+      repository(owner: $owner, name: $name) {
+        object(expression: $expression) {
+          ... on Tree {
+            entries {
+              name
+              object {
+                ... on Blob {
+                  text
+                }
+              }
+            }
+          }
+          ... on Blob {
+            text
+          }
+        }
+      }
+    }
+  `;
+
+  const variables = {
+    owner: "iammatthias",
+    name: "obsidian_cms",
+    expression,
+  };
+
   const response = await fetch("https://api.github.com/graphql", {
     method: "POST",
     headers: {
@@ -33,42 +93,12 @@ async function fetchFromGitHubGraphQL(query: string, variables: any) {
 }
 
 // Function to get all entries
-export async function getObsidianEntries(path: string, slug?: string) {
-  // Adjust the expression based on whether a slug is provided
-  const expression = slug ? `HEAD:content/${path}/${slug}.md` : `HEAD:content/${path}`;
-
+export async function getObsidianEntries(path?: string, slug?: string) {
   const {
     data: {
       repository: { object },
     },
-  } = await fetchFromGitHubGraphQL(
-    `
-      query fetchEntries($owner: String!, $name: String!, $expression: String!) {
-        repository(owner: $owner, name: $name) {
-          object(expression: $expression) {
-            ... on Tree {
-              entries {
-                name
-                object {
-                  ... on Blob {
-                    text
-                  }
-                }
-              }
-            }
-            ... on Blob {
-              text
-            }
-          }
-        }
-      }
-    `,
-    {
-      owner: "iammatthias",
-      name: "obsidian_cms",
-      expression,
-    }
-  );
+  } = await fetchFromGitHubGraphQL(path, slug);
 
   // For single entry (when slug is provided)
   if (slug) {
@@ -92,7 +122,71 @@ export async function getObsidianEntries(path: string, slug?: string) {
     })
   );
 
-  parseAndMergeTags(parsedEntries); // Assuming this is for bulk entries
+  // parseAndMergeTags(parsedEntries); // Assuming this is for bulk entries
 
   return parsedEntries;
+}
+
+export async function getObsidianTags() {
+  const {
+    data: {
+      repository: { object: paths },
+    },
+  } = await fetchFromGitHubGraphQL();
+  const extractedPaths = paths.entries.map((entry) => entry.name);
+
+  for (const path of extractedPaths) {
+    const {
+      data: {
+        repository: { object },
+      },
+    } = await fetchFromGitHubGraphQL(path);
+
+    for (const entry of object.entries) {
+      const content = entry.object.text;
+      const parsedContent = await parseMarkdownContent(content, path);
+      const { title, slug, tags } = parsedContent.frontmatter;
+
+      // Check if post already exists
+      const existingPost = await db.select().from(Posts).where(eq(Posts.slug, slug)).execute();
+
+      let postId;
+      if (existingPost.length > 0) {
+        // Update the post if it exists
+        await db.update(Posts).set({ title }).where(eq(Posts.slug, slug)).execute();
+        postId = existingPost[0].id;
+      } else {
+        // Insert new post if it doesn't exist
+        const postInsertionResult = await db.insert(Posts).values({ title, slug, path }).execute();
+        postId = Number(postInsertionResult.lastInsertRowid);
+      }
+
+      // Handle tags
+      for (let i = 0; i < tags.length; i++) {
+        const existingTag = await db.select().from(Tags).where(eq(Tags.name, tags[i])).execute();
+
+        let tagId;
+        if (existingTag.length > 0) {
+          // If tag exists, get its id
+          tagId = existingTag[0].id;
+        } else {
+          // If tag doesn't exist, insert it and get its id
+          const tagInsertionResult = await db.insert(Tags).values({ name: tags[i] }).execute();
+          tagId = Number(tagInsertionResult.lastInsertRowid);
+        }
+
+        // Check if post-tag relationship already exists
+        const existingPostTag = await db
+          .select()
+          .from(PostTags)
+          .where(and(eq(PostTags.postId, postId), eq(PostTags.tagId, tagId)))
+          .execute();
+
+        if (existingPostTag.length === 0) {
+          // If post-tag relationship doesn't exist, insert it
+          await db.insert(PostTags).values({ postId, tagId }).execute();
+        }
+      }
+    }
+  }
 }
