93298ec835ca21aabf1074a872327e1f91b2710e
iammatthias <me@iammatthias.com>
2024-04-06 13:16:01 -0700

    Refactor nonce handling and implement transaction queue

--- src/pages/api/increment-page-view.ts
+++ src/pages/api/increment-page-view.ts
@@ -2,37 +2,60 @@ import { publicClient, walletClient } from "@lib/viemClients";
 import { privateKeyToAccount } from "viem/accounts";
 import { addSessionABI } from "@lib/abi";
 
-let nonce = null; // Add a nonce variable
+let baseNonce = null; // This will store the initial nonce
+let transactionQueue: Array<() => Promise<void>> = []; // Initialize an empty array to hold the transaction queue
+let processingQueue = false;
+let account; // Initialize account variable
 
 export async function POST({ request }) {
+  if (!account) {
+    // Ensure the private key is only used here for initializing the account
+    account = privateKeyToAccount(`0x${import.meta.env.HIT_COUNTER_WALLET}`);
+  }
+
   const requestBody = await request.json();
-
-  const sessionHash = await requestBody.sessionHash;
-
+  const sessionHash = requestBody.sessionHash; // Await is not necessary here
   const contractAddress = await import.meta.env.PUBLIC_HIT_COUNTER_CONTRACT;
 
-  const account = privateKeyToAccount(
-    `0x${import.meta.env.HIT_COUNTER_WALLET}`,
-  );
+  if (baseNonce === null) {
+    baseNonce = await publicClient.getTransactionCount({
+      address: account.address,
+      blockTag: "latest", // Adjust based on your requirement (pending/latest)
+    });
+  }
 
-  const { request: contractRequest } = await publicClient.simulateContract({
-    address: `0x${contractAddress}`,
-    abi: addSessionABI!,
-    functionName: "addSession",
-    args: [sessionHash],
-    account,
+  transactionQueue.push(async () => {
+    // Calculate the current nonce for this transaction
+    const currentNonce = baseNonce!++;
+
+    const { request: contractRequest } = await publicClient.simulateContract({
+      address: `0x${contractAddress}`,
+      abi: addSessionABI!,
+      functionName: "addSession",
+      args: [sessionHash],
+      account,
+      nonce: currentNonce, // Use the incremented nonce
+    });
+
+    await walletClient.writeContract(contractRequest);
   });
 
-  // Always fetch the latest nonce from the blockchain
-  const nonce = await publicClient.getTransactionCount({
-    address: account.address,
-  });
+  if (!processingQueue) {
+    processQueue();
+  }
 
-  // Add the nonce to the transaction
-  contractRequest.nonce = nonce;
-
-  await walletClient.writeContract(contractRequest);
   return new Response(JSON.stringify({ status: "OK" }), {
     headers: { "Content-Type": "application/json" },
   });
+}
+
+async function processQueue() {
+  processingQueue = true;
+  while (transactionQueue.length > 0) {
+    const transaction = transactionQueue.shift();
+    if (transaction) {
+      await transaction();
+    }
+  }
+  processingQueue = false;
 }
