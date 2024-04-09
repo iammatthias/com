198bba30f3e50490f05f3b8f536d4048ef25b9a1
iammatthias <me@iammatthias.com>
2024-04-09 08:52:42 -0700

    Simplify increment-hit-counter API endpoint for troubleshooting

--- src/pages/api/increment-hit-counter.ts
+++ src/pages/api/increment-hit-counter.ts
@@ -1,69 +1,44 @@
 import { publicClient, walletClient } from "@lib/viemClients";
 import { privateKeyToAccount } from "viem/accounts";
 import { addSessionABI } from "@lib/abi";
-import { Mutex } from "async-mutex";
-
-const mutex = new Mutex();
-let nonce = null;
 
 export async function POST({ request }) {
   try {
     const requestBody = await request.json();
-    const sessionHash = requestBody.sessionHash;
-    const contractAddress = import.meta.env.PUBLIC_HIT_COUNTER_CONTRACT;
-    const privateKey = import.meta.env.HIT_COUNTER_WALLET;
+    const sessionHash = await requestBody.sessionHash;
+    const contractAddress = await import.meta.env.PUBLIC_HIT_COUNTER_CONTRACT;
 
-    if (!sessionHash || !contractAddress || !privateKey) {
-      console.error("Missing required parameters");
-      return new Response(
-        JSON.stringify({
-          status: "Error",
-          message: "Missing required parameters",
-        }),
-        {
-          status: 400,
-          headers: { "Content-Type": "application/json" },
-        },
-      );
-    }
+    console.log(`Received request to add session with hash: ${sessionHash}`);
 
-    const account = privateKeyToAccount(`0x${privateKey}`);
+    const account = privateKeyToAccount(
+      `0x${import.meta.env.HIT_COUNTER_WALLET}`,
+    );
+
+    console.log(`Using account: ${account.address}`);
 
     const { request: contractRequest } = await publicClient.simulateContract({
       address: `0x${contractAddress}`,
-      abi: addSessionABI,
+      abi: addSessionABI!,
       functionName: "addSession",
       args: [sessionHash],
       account,
     });
 
-    const release = await mutex.acquire();
-    try {
-      if (nonce === null) {
-        nonce = await publicClient.getTransactionCount({
-          address: account.address,
-          blockTag: "latest",
-        });
-      } else {
-        nonce++;
-      }
+    console.log(
+      `Simulated contract request: ${JSON.stringify(contractRequest)}`,
+    );
 
-      contractRequest.nonce = nonce;
-    } finally {
-      release();
-    }
+    const result = await walletClient.writeContract(contractRequest);
 
-    const writeContractResponse =
-      await walletClient.writeContract(contractRequest);
-    console.log("Transaction hash:", writeContractResponse);
+    console.log(`Transaction sent. Transaction hash: ${result.hash}`);
 
     return new Response(JSON.stringify({ status: "OK" }), {
       headers: { "Content-Type": "application/json" },
     });
   } catch (error) {
-    console.error("Error processing request:", error);
+    console.error(`Error adding session: ${error.message}`);
     return new Response(
-      JSON.stringify({ status: "Error", message: "An error occurred" }),
+      JSON.stringify({ status: "Error", error: error.message }),
       {
         status: 500,
         headers: { "Content-Type": "application/json" },
