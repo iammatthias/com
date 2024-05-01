69d82b2059055a0fdc2564629e880455fa6d3780
iammatthias <me@iammatthias.com>
2024-05-01 16:50:03 -0700

    Add playformCompress integration to astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,53 +1,53 @@
 import { defineConfig } from "astro/config";
 import alpine from "@astrojs/alpinejs";
 import vercel from "@astrojs/vercel/serverless";
-
 import metaTags from "astro-meta-tags";
 import devtoolBreakpoints from "astro-devtool-breakpoints";
-
 import embeds from "astro-embed/integration";
+
+import playformCompress from "@playform/compress";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://iammatthias.com",
-  integrations: [alpine(), embeds(), metaTags(), devtoolBreakpoints()],
+  integrations: [alpine(), embeds(), metaTags(), devtoolBreakpoints(), playformCompress()],
   output: "hybrid",
   adapter: vercel(),
   prefetch: {
-    prefetchAll: true,
+    prefetchAll: true
   },
   redirects: {
     "./post/1563778800000": {
       status: 302,
-      destination: "./notes/1563778800000-flatframe",
+      destination: "./notes/1563778800000-flatframe"
     },
     "./post/1587970800000": {
       status: 302,
-      destination: "./notes/1587970800000-sourdough",
+      destination: "./notes/1587970800000-sourdough"
     },
     "./post/1687071600000": {
       status: 302,
-      destination: "./notes/1687071600000-feels-like-summer",
+      destination: "./notes/1687071600000-feels-like-summer"
     },
     "./post/1681369200000": {
       status: 302,
-      destination: "./posts/1681369200000-ai-legion-on-bluesky",
+      destination: "./posts/1681369200000-ai-legion-on-bluesky"
     },
     "./post/1670659200001": {
       status: 302,
-      destination: "./posts/1670659200001-obsidian-as-a-cms",
+      destination: "./posts/1670659200001-obsidian-as-a-cms"
     },
     "./post/1691436904927": {
       status: 302,
-      destination: "./posts/1691436904927-deploy-html-on-replit",
+      destination: "./posts/1691436904927-deploy-html-on-replit"
     },
     "./post/1699332127006": {
       status: 302,
-      destination: "./posts/1699332127006-revisiting-obsidian-as-a-cms",
+      destination: "./posts/1699332127006-revisiting-obsidian-as-a-cms"
     },
     "./post/*": {
       status: 302,
-      destination: "./posts",
-    },
-  },
+      destination: "./posts"
+    }
+  }
 });
