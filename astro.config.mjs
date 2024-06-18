7b57b3718dd99861cdfe6391a6734635218c26b5
iammatthias <me@iammatthias.com>
2024-06-17 17:40:24 -0700

    chore: Update redirects for blog post URLs

--- astro.config.mjs
+++ astro.config.mjs
@@ -20,37 +20,37 @@ export default defineConfig({
     prefetchAll: true,
   },
   redirects: {
-    "./post/1563778800000": {
+    "/post/1563778800000": {
       status: 302,
-      destination: "./notes/1563778800000-flatframe",
+      destination: "/notes/1563778800000-flatframe",
     },
-    "./post/1587970800000": {
+    "/post/1587970800000": {
       status: 302,
-      destination: "./notes/1587970800000-sourdough",
+      destination: "/notes/1587970800000-sourdough",
     },
-    "./post/1687071600000": {
+    "/post/1687071600000": {
       status: 302,
-      destination: "./notes/1687071600000-feels-like-summer",
+      destination: "/notes/1687071600000-feels-like-summer",
     },
-    "./post/1681369200000": {
+    "/post/1681369200000": {
       status: 302,
-      destination: "./posts/1681369200000-ai-legion-on-bluesky",
+      destination: "/posts/1681369200000-ai-legion-on-bluesky",
     },
-    "./post/1670659200001": {
+    "/post/1670659200001": {
       status: 302,
-      destination: "./posts/1670659200001-obsidian-as-a-cms",
+      destination: "/posts/1670659200001-obsidian-as-a-cms",
     },
-    "./post/1691436904927": {
+    "/post/1691436904927": {
       status: 302,
-      destination: "./posts/1691436904927-deploy-html-on-replit",
+      destination: "/posts/1691436904927-deploy-html-on-replit",
     },
-    "./post/1699332127006": {
+    "/post/1699332127006": {
       status: 302,
-      destination: "./posts/1699332127006-revisiting-obsidian-as-a-cms",
+      destination: "/posts/1699332127006-revisiting-obsidian-as-a-cms",
     },
-    "./post/*": {
+    "/post/*": {
       status: 302,
-      destination: "./posts",
+      destination: "/posts",
     },
   },
 });
