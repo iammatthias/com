6e80a083101cf87e5366ca128a97400e6b913afa
iammatthias <me@iammatthias.com>
2024-10-23 11:31:25 -0700

    Refactor gitignore and page layouts, update dependencies, and remove unused code

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,11 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 
-import sitemap from "@astrojs/sitemap";
-
-import robotsTxt from "astro-robots-txt";
-
 import vercel from "@astrojs/vercel/static";
 
 // https://astro.build/config
 export default defineConfig({
   output: "static",
-  integrations: [sitemap(), robotsTxt()],
+
   adapter: vercel(),
 });
