d5579cd97fc94a15a2f4132226c51745f4b03bb1
iammatthias <me@iammatthias.com>
2024-10-24 12:18:37 -0700

    Refactor output path to "static" in astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,6 @@ import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
-  output: "server",
-
+  output: "static",
   adapter: vercel(),
 });
