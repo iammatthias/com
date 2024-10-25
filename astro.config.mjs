0053592f3ef77ba10aec67645ca9f89cf3d0a04d
iammatthias <me@iammatthias.com>
2024-10-25 10:24:58 -0700

    Refactor astro.config.mjs to add site URL and update redirects

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,52 @@ import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://iammatthias.com",
   output: "static",
   adapter: vercel(),
+  prefetch: {
+    prefetchAll: true,
+  },
+  redirects: {
+    "/post/1563778800000": {
+      status: 302,
+      destination: "/notes/1563778800000-flatframe",
+    },
+    "/post/1587970800000": {
+      status: 302,
+      destination: "/notes/1587970800000-sourdough",
+    },
+    "/post/1687071600000": {
+      status: 302,
+      destination: "/notes/1687071600000-feels-like-summer",
+    },
+    "/post/1681369200000": {
+      status: 302,
+      destination: "/posts/1681369200000-ai-legion-on-bluesky",
+    },
+    "/post/1670659200001": {
+      status: 302,
+      destination: "/posts/1670659200001-obsidian-as-a-cms",
+    },
+    "/post/1691436904927": {
+      status: 302,
+      destination: "/posts/1691436904927-deploy-html-on-replit",
+    },
+    "/post/1699332127006": {
+      status: 302,
+      destination: "/posts/1699332127006-revisiting-obsidian-as-a-cms",
+    },
+    "/post/*": {
+      status: 302,
+      destination: "/posts",
+    },
+    "/content/*": {
+      status: 302,
+      destination: "/posts",
+    },
+    "/md/*": {
+      status: 302,
+      destination: "/posts",
+    },
+  },
 });
