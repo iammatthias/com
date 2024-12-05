2253e8f2e8d532ed1741f1e0ec184b36bff03534
iammatthias <me@iammatthias.com>
2024-12-05 07:22:49 -0800

    Update Astro dependencies and Vercel adapter configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 
-import vercel from "@astrojs/vercel/serverless";
+import vercel from "@astrojs/vercel";
 
 // https://astro.build/config
 export default defineConfig({
